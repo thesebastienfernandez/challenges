2bbb9484b4e84d41341df66806858a6e7669710c
sebastienfernandez <sebastienfernandez@protonmail.com>
2025-01-30 01:33:51 +0100

    ajout d'images et des défis + structure de base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,32 @@
 
 <script>
     import '@picocss/pico';
+    import Card from './Card.svelte';
+    import challenges from "/src/challenges.json"
+    let name = "Seb"
+    let subtitle = `More difficulties for more <strong> fun !</strong>` 
+    let src = "/challenges-wallpaper2.jpg"
+    let points = $state(0)
+    const addPoint = () => {
+        points ++
+    }
+    console.log(challenges.data)
 </script>
-<h1>Lethal Company Challenges</h1>
-<button>Mon bouton</button>
+<hgroup>
+    <h1 class="pico-color-cyan-200">Lethal Company Challenges</h1>
+    <p>{@html subtitle}</p>
+</hgroup>
+<p>Points de {name.toLocaleUpperCase()} : {points}</p>
+<button onclick={addPoint}> + </button>
+{#each challenges.data as challenge, index}
+<Card title={challenge.title} description={challenge.description}/>
+{/each}
+<img src={src} alt="lethal company wallpaper" />
+<main>
+    
+</main>
 <style>
-
-
+p {
+    font-style: italic;
+}
 </style>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,10 @@
+<button>
+    <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </div>
+</button>
+
+<script>
+let {title, description} = $props()
+</script>
